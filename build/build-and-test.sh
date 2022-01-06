4d5d51bc345ebf6ea68d7f42291b89e9b5ccd3ff
Nicolas Fournier <nicolas.fournier@posteo.net>
2022-01-06 21:38:23 +0100

    Simplification du workflow et création d'un script de build

---
+++ build/build-and-test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eux
+
+if [ ! -f .env ]; then
+    echo "\
+        # DATABASE
+        DB_NAME=exampledb
+        DB_USER=exampleuser
+        DB_PASSWORD=examplepass
+
+        # WORDPRESS
+        ADMINUSER=adminUser
+        ADMINPASSWORD=secret
+        ADMINEMAIL=toto@titi.net" > .env
+fi
+
+docker compose build
+
+docker compose up -d
+
+sleep 30
+
+docker compose run --rm wp sh /usr/src/wordpress/scripts/wp-test.sh
+
