55ecfcd9c6db701db712a33bd9a67f5ab764d165
Nicolas Fournier <nicolas.fournier@posteo.net>
2022-01-08 16:45:11 +0100

    Création d'artefacts

--- build/build-and-test.sh
+++ build/build-and-test.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Valeurs par défaut 
+if [ -z ${ARTIFACTS_DIR+x} ]; then
+    ARTIFACTS_DIR=/tmp/mon-cv-artifacts
+fi
+
+if [ -z ${SAVE_ARTIFACTS+x} ]; then
+    SAVE_ARTIFACTS=false
+fi
+
+# Script
 set -eux
 
 if [ ! -f .env ]; then
@@ -23,3 +33,9 @@ docker compose up -d
 sleep 30
 
 docker compose run --rm wp sh /usr/src/wordpress/scripts/wp-test.sh
+
+if [ "$SAVE_ARTIFACTS" = "true" ]; then
+    mkdir -p "$ARTIFACTS_DIR"
+    docker save inconico/mon-cv-wordpress -o "$ARTIFACTS_DIR/mon-cv-wordpress.tar"
+    docker save inconico/mon-cv-wp -o  "$ARTIFACTS_DIR/mon-cv-wp.tar"
+fi
