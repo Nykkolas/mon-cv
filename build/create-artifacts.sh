13a147b65af446473e18a2863791377908d1dac5
Nicolas Fournier <nicolas.fournier@posteo.net>
2022-01-08 17:48:21 +0100

    erreur de frappe

--- build/create-artifacts.sh
+++ build/create-artifacts.sh
@@ -9,5 +9,5 @@ fi
 set -eux
 
 mkdir -p "$ARTIFACTS_DIR"
-docker save ghcr.io/inconico/mon-cv-wordpress -o "$ARTIFACTS_DIR/mon-cv-wordpress.tar"
-docker save ghcr.io/inconico/mon-cv-wp -o  "$ARTIFACTS_DIR/mon-cv-wp.tar"
+docker save ghcr.io/nykkolas/mon-cv-wordpress -o "$ARTIFACTS_DIR/mon-cv-wordpress.tar"
+docker save ghcr.io/nykkolas/mon-cv-wp -o  "$ARTIFACTS_DIR/mon-cv-wp.tar"
