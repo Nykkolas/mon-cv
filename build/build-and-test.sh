8684932931dd110bbc1695a94c7112082b2a1dd1
Nicolas Fournier <nicolas.fournier@posteo.net>
2022-01-12 22:16:47 +0100

    docker compse en fait c'est mieux

--- build/build-and-test.sh
+++ build/build-and-test.sh
@@ -16,13 +16,13 @@ if [ ! -s .env ]; then
         ADMINEMAIL=toto@titi.net" > .env
 fi
 
-docker-compose build
+docker compose build
 
-docker-compose up -d
+docker compose up -d
 
-until docker-compose ps wp | grep exited >/dev/null 2>&1; do
+until docker compose ps wp | grep exited >/dev/null 2>&1; do
     echo "Waiting end of wp-init..."
     sleep 1
 done
 
-docker-compose run --rm wp sh /usr/src/wordpress/scripts/wp-test.sh
+docker compose run --rm wp sh /usr/src/wordpress/scripts/wp-test.sh
--- build/dump-static-content.sh
+++ build/dump-static-content.sh
@@ -6,20 +6,20 @@ set -eux
 # TODO : Option -f pour forcer le dump même si la poubelle n'est pas vide
 # TODO : Option -e pour vider la poubelle (e comme Empty trash)
 
-docker-compose run --rm wp config push all_options \
-    && docker-compose cp wordpress:/var/www/html/wp-content/config/all_options.json ./wordpress/config
+docker compose run --rm wp config push all_options \
+    && docker compose cp wordpress:/var/www/html/wp-content/config/all_options.json ./wordpress/config
 
-docker-compose run --rm wp export \
+docker compose run --rm wp export \
     --post_type=page,nav_menu_item \
     --stdout \
     > wp/exports/all.xml
 
-# docker-compose run --rm wp export \
+# docker compose run --rm wp export \
 #     --post_type=page \
 #     --stdout \
 #     > wp/exports/pages.xml
 
-# docker-compose run --rm wp export \
+# docker compose run --rm wp export \
 #     --post_type=nav_menu_item \
 #     --stdout \
 #     > wp/exports/menus.xml
