245829c54590c1f3e03170843af11633169aafc9
Nicolas Fournier <nicolas.fournier@posteo.net>
2022-01-13 07:18:51 +0100

    mauvais nom de fichier docker-compose-yml

--- build/create-package.sh
+++ build/create-package.sh
@@ -9,7 +9,7 @@ SOURCE_FOLDER=${PWD##*/}
 echo $TAG > tag
 
 tar -C .. -cvf release/$SOURCE_FOLDER-$TAG.tar \
-    $SOURCE_FOLDER/docker compose.yml \
+    $SOURCE_FOLDER/docker-compose.yml \
     $SOURCE_FOLDER/env \
     $SOURCE_FOLDER/build/deploy.sh \
     $SOURCE_FOLDER/tag \
