81f64b21db9d591dab56a367eb42f91a5fac69c1
Nicolas Fournier <nicolas.fournier@posteo.net>
2022-01-13 08:29:56 +0100

    Aide pour utiliser ce fichier

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
-set -eux
+set -eu
+
+usage () {
+    echo "This script pulls, builds and starts the containers."
+    echo "It requires 2 files : "
+    echo "  - .env with the various parameters (see example folder)"
+    echo "  - tag containing the version to deploy"
+    echo ""
+    echo "Usage : sh $0"
+}
+
+if [ "-h" = $1 ]; then
+    usage
+    exit 0
+fi
+
 
 if [ -s tag ]; then
     export TAG=$(cat tag)
 else
     echo "ERROR : no \"tag\" file available"
+    usage
     exit 1
 fi
 
 if [ ! -s .env ]; then
     echo "ERROR : Merci de créer un fichier .env"
+    usage
     exit 1
 else
     docker compose pull
